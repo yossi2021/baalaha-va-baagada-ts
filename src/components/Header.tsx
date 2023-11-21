a869df3bc2f00fe1d10b0d1b8173bd3c70a055e8
yosef <yossibcash@gmail.com>
2023-11-21 19:36:44 +0200

    fix toggler

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,16 +13,16 @@ function Header() {
         <header className=" position-fixed z-3" >
     <nav className="navbar navbar-expand-lg bg-white ">
         <div className="display-grid ">
-        <button className="navbar-toggler m-1 " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler m-2 z-2 rounded-pill " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="text-dark ">בהלכה ובאגדה</span>
             </button>
             {/* <a className="navbar-brand " href="./">
                 <i className="bi-book me-2"> </i>
                     בהלכה ובאגדה
             </a> */}
             <div>
-            <div className="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
+            <div className="collapse navbar-collapse px-2" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center ">
                 <li className="nav-item ms-1 ">
                     <NavLink                         
                         className="nav-link"
@@ -125,7 +125,7 @@ function Header() {
             </div>
 
         </div>
-        <p className=" text-light bg-dark px-2 rounded-pill p-1 m-1">בהלכה ובאגדה</p>
+        {/* <p className=" text-light bg-dark px-2 rounded-pill p-1 m-1">בהלכה ובאגדה</p> */}
 
         <User/>
 
